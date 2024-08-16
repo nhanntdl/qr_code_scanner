2f02edc7b5bc074010bee18258ea4f4a45e0f2bd
nhanntsosene <nhannt@sosene.asia>
2024-08-16 14:34:02 +0700

    update overlay layout

---
+++ lib/src/qr_scanner_overlay_shape.dart
@@ -0,0 +1,252 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class QrScannerOverlayShape extends ShapeBorder {
+  QrScannerOverlayShape({
+    this.borderColor = Colors.white,
+    this.borderWidth = 20.0,
+    this.overlayColor = const Color.fromRGBO(0, 0, 0, 80),
+    this.borderRadius = 10,
+    this.borderLength = 30,
+    double? cutOutSize,
+    double? cutOutWidth,
+    double? cutOutHeight,
+    this.cutOutBottomOffset = 0,
+  })  : cutOutWidth = cutOutWidth ?? cutOutSize ?? 250,
+        cutOutHeight = cutOutHeight ?? cutOutSize ?? 250 {
+    assert(
+      borderLength <=
+          min(this.cutOutWidth, this.cutOutHeight) / 2 + borderWidth * 2,
+      "Border can't be larger than ${min(this.cutOutWidth, this.cutOutHeight) / 2 + borderWidth * 2}",
+    );
+    assert(
+        (cutOutWidth == null && cutOutHeight == null) ||
+            (cutOutSize == null && cutOutWidth != null && cutOutHeight != null),
+        'Use only cutOutWidth and cutOutHeight or only cutOutSize');
+  }
+
+  final Color borderColor;
+  final double borderWidth;
+  final Color overlayColor;
+  final double borderRadius;
+  final double borderLength;
+  final double cutOutWidth;
+  final double cutOutHeight;
+  final double cutOutBottomOffset;
+
+  @override
+  EdgeInsetsGeometry get dimensions => const EdgeInsets.all(10);
+
+  get cutOutSize => null;
+
+  @override
+  Path getInnerPath(Rect rect, {TextDirection? textDirection}) {
+    return Path()
+      ..fillType = PathFillType.evenOdd
+      ..addPath(getOuterPath(rect), Offset.zero);
+  }
+
+  @override
+  Path getOuterPath(Rect rect, {TextDirection? textDirection}) {
+    Path _getLeftTopPath(Rect rect) {
+      return Path()
+        ..moveTo(rect.left, rect.bottom)
+        ..lineTo(rect.left, rect.top)
+        ..lineTo(rect.right, rect.top);
+    }
+
+    return _getLeftTopPath(rect)
+      ..lineTo(
+        rect.right,
+        rect.bottom,
+      )
+      ..lineTo(
+        rect.left,
+        rect.bottom,
+      )
+      ..lineTo(
+        rect.left,
+        rect.top,
+      );
+  }
+
+  @override
+  void paint(Canvas canvas, Rect rect, {TextDirection? textDirection}) {
+    final width = rect.width;
+    final borderWidthSize = width / 2;
+    final height = rect.height;
+    final borderOffset = borderWidth / 2;
+    final _borderLength =
+        borderLength > min(cutOutHeight, cutOutHeight) / 2 + borderWidth * 2
+            ? borderWidthSize / 2
+            : borderLength;
+    final _cutOutWidth =
+        cutOutWidth < width ? cutOutWidth : width - borderOffset;
+    final _cutOutHeight =
+        cutOutHeight < height ? cutOutHeight : height - borderOffset;
+
+    final backgroundPaint = Paint()
+      ..color = overlayColor
+      ..style = PaintingStyle.fill;
+
+    final borderPaint = Paint()
+      ..color = borderColor
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = borderWidth
+      ..strokeCap = StrokeCap.round;
+
+    final boxPaint = Paint()
+      ..color = borderColor
+      ..style = PaintingStyle.fill
+      ..blendMode = BlendMode.dstOut;
+
+    final cutOutRect = Rect.fromLTWH(
+      rect.left + width / 2 - _cutOutWidth / 2 + borderOffset,
+      -cutOutBottomOffset +
+          rect.top +
+          height / 2 -
+          _cutOutHeight / 2 +
+          borderOffset,
+      _cutOutWidth - borderOffset * 2,
+      _cutOutHeight - borderOffset * 2,
+    );
+
+    var capPaint = Paint()..color = Colors.white;
+
+    canvas
+      ..saveLayer(
+        rect,
+        backgroundPaint,
+      )
+      // Draw top right corner
+      ..drawRRect(
+        RRect.fromLTRBAndCorners(
+          cutOutRect.right - _borderLength,
+          cutOutRect.top,
+          cutOutRect.right,
+          cutOutRect.top + _borderLength,
+          topRight: Radius.circular(borderRadius),
+        ),
+        borderPaint,
+      )
+      // Draw top left caps
+      ..drawCircle(
+        Offset(
+          cutOutRect.right - _borderLength - borderWidth / 2,
+          cutOutRect.top - borderWidth / 4,
+        ),
+        borderWidth / 4,
+        capPaint,
+      )
+      ..drawCircle(
+        Offset(
+          cutOutRect.right + borderWidth / 4,
+          cutOutRect.top + _borderLength + borderWidth / 2,
+        ),
+        borderWidth / 4,
+        capPaint,
+      )
+      // Draw top left corner
+      ..drawRRect(
+        RRect.fromLTRBAndCorners(
+          cutOutRect.left,
+          cutOutRect.top,
+          cutOutRect.left + _borderLength,
+          cutOutRect.top + _borderLength,
+          topLeft: Radius.circular(borderRadius),
+        ),
+        borderPaint,
+      )
+      // Draw top left caps
+      ..drawCircle(
+        Offset(
+          cutOutRect.left - borderWidth / 4,
+          cutOutRect.top + _borderLength + borderWidth / 2,
+        ),
+        borderWidth / 4,
+        capPaint,
+      )
+      ..drawCircle(
+        Offset(
+          cutOutRect.left + _borderLength + borderWidth / 2,
+          cutOutRect.top - borderWidth / 4,
+        ),
+        borderWidth / 4,
+        capPaint,
+      )
+      // Draw bottom right corner
+      ..drawRRect(
+        RRect.fromLTRBAndCorners(
+          cutOutRect.right - _borderLength,
+          cutOutRect.bottom - _borderLength,
+          cutOutRect.right,
+          cutOutRect.bottom,
+          bottomRight: Radius.circular(borderRadius),
+        ),
+        borderPaint,
+      )
+      // Draw bottom right caps
+      ..drawCircle(
+        Offset(
+          cutOutRect.right + borderWidth / 4,
+          cutOutRect.bottom - _borderLength - borderWidth / 2,
+        ),
+        borderWidth / 4,
+        capPaint,
+      )
+      ..drawCircle(
+        Offset(
+          cutOutRect.right - _borderLength - borderWidth / 2,
+          cutOutRect.bottom + borderWidth / 4,
+        ),
+        borderWidth / 4,
+        capPaint,
+      )
+      // Draw bottom left corner
+      ..drawRRect(
+        RRect.fromLTRBAndCorners(
+          cutOutRect.left,
+          cutOutRect.bottom - _borderLength,
+          cutOutRect.left + _borderLength,
+          cutOutRect.bottom,
+          bottomLeft: Radius.circular(borderRadius),
+        ),
+        borderPaint,
+      )
+      // Draw bottom left caps
+      ..drawCircle(
+        Offset(
+          cutOutRect.left - borderWidth / 4,
+          cutOutRect.bottom - _borderLength - borderWidth / 2,
+        ),
+        borderWidth / 4,
+        capPaint,
+      )
+      ..drawCircle(
+        Offset(
+          cutOutRect.left + _borderLength + borderWidth / 2,
+          cutOutRect.bottom + borderWidth / 4,
+        ),
+        borderWidth / 4,
+        capPaint,
+      )
+      ..drawRRect(
+        RRect.fromRectAndRadius(
+          cutOutRect,
+          Radius.circular(borderRadius),
+        ),
+        boxPaint,
+      )
+      ..restore();
+  }
+
+  @override
+  ShapeBorder scale(double t) {
+    return QrScannerOverlayShape(
+      borderColor: borderColor,
+      borderWidth: borderWidth,
+      overlayColor: overlayColor,
+    );
+  }
+}
